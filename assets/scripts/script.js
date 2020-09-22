9901e7694bb8d1a2f15ee2c332758d03df5f2013
Juan-CamiloF <jucamilogac@outlook.com>
2020-09-22 17:28:18 -0500

    Definitivo

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -2,7 +2,6 @@ const tecla = document.querySelectorAll('.tecla')
 const barraLetras = document.getElementById('barraLetras')
 const barraNumeros = document.getElementById('barraNumeros')
 
-
 barraLetras.innerText = localStorage.getItem('letras')
 barraNumeros.innerText = localStorage.getItem('numeros')
 
@@ -14,11 +13,11 @@ const teclado = (event) => {
                 barraLetras.innerText = 'A'
                 localStorage.setItem('letras', barraLetras.innerText)
             } else if (barraLetras.innerText == 'A') {
-                alert('Ya esta la letra A')
+                swal('Ya esta la letra A')
             }
             if (localStorage.getItem('letras') == 'ABCDEFGHIJ') {
                 if (barraNumeros.innerText == '1') {
-                    alert('El 1 ya esta')
+                    swal('El 1 ya esta')
                 } else if (barraNumeros.innerText == '') {
                     barraNumeros.innerText += '1';
                     localStorage.setItem('numeros', barraNumeros.innerText)
@@ -27,149 +26,172 @@ const teclado = (event) => {
             break;
         case 'B-2':
             if (barraLetras.innerText == 'AB') {
-                alert('Ya esta la B')
+                swal('Ya esta la B')
             } else if (barraLetras.innerText == 'A') {
                 barraLetras.innerText += 'B'
                 localStorage.setItem('letras', barraLetras.innerText)
             } else if (barraLetras.innerText == '') {
-                alert('Falta la letra A')
+                swal('Falta la letra A')
             }
             if (localStorage.getItem('letras') == 'ABCDEFGHIJ') {
                 if (barraNumeros.innerText == '12') {
-                    alert('El 2 ya esta')
+                    swal('El 2 ya esta')
                 } else if (barraNumeros.innerText == '1') {
                     barraNumeros.innerText += '2';
                     localStorage.setItem('numeros', barraNumeros.innerText)
+                }
+                else{
+                    swal('Falta el 1')
                 }
             }
             break;
         case 'C-3':
             if (barraLetras.innerText == 'ABC') {
-                alert('Ya esta la C')
+                swal('Ya esta la C')
             } else if (barraLetras.innerText == 'AB') {
                 barraLetras.innerText += 'C'
                 localStorage.setItem('letras', barraLetras.innerText)
             } else if (barraLetras.innerText == '' || barraLetras.innerText == 'A') {
-                alert('Falta la letra AB')
+                swal('Falta la letra AB')
             }
             if (localStorage.getItem('letras') == 'ABCDEFGHIJ') {
                 if (barraNumeros.innerText == '123') {
-                    alert('El 3 ya esta')
+                    swal('El 3 ya esta')
                 } else if (barraNumeros.innerText == '12') {
                     barraNumeros.innerText += '3';
                     localStorage.setItem('numeros', barraNumeros.innerText)
+                }
+                else{
+                    swal('Falta el 12')
                 }
             }
             break;
         case 'D-4':
             if (barraLetras.innerText == 'ABCD') {
-                alert('Ya esta la D')
+                swal('Ya esta la D')
             } else if (barraLetras.innerText == 'ABC') {
                 barraLetras.innerText += 'D'
                 localStorage.setItem('letras', barraLetras.innerText)
             } else if (barraLetras.innerText == '' || barraLetras.innerText == 'A' || barraLetras.innerText == 'AB') {
-                alert('Falta la letras ABC')
+                swal('Falta la letras ABC')
             }
             if (localStorage.getItem('letras') == 'ABCDEFGHIJ') {
                 if (barraNumeros.innerText == '1234') {
-                    alert('El 4 ya esta')
+                    swal('El 4 ya esta')
                 } else if (barraNumeros.innerText == '123') {
                     barraNumeros.innerText += '4';
                     localStorage.setItem('numeros', barraNumeros.innerText)
+                }
+                else{
+                    swal('Falta el 123')
                 }
             }
             break;
         case 'E-5':
             if (barraLetras.innerText == 'ABCDE') {
-                alert('Ya esta la E')
+                swal('Ya esta la E')
             } else if (barraLetras.innerText == 'ABCD') {
                 barraLetras.innerText += 'E'
                 localStorage.setItem('letras', barraLetras.innerText)
             } else if (barraLetras.innerText == '' || barraLetras.innerText == 'A' || barraLetras.innerText == 'AB' || barraLetras.innerText == 'ABC') {
-                alert('Falta la letras ABCD')
+                swal('Falta la letras ABCD')
             }
             if (localStorage.getItem('letras') == 'ABCDEFGHIJ') {
                 if (barraNumeros.innerText == '12345') {
-                    alert('El 5 ya esta')
+                    swal('El 5 ya esta')
                 } else if (barraNumeros.innerText == '1234') {
                     barraNumeros.innerText += '5';
                     localStorage.setItem('numeros', barraNumeros.innerText)
+                }
+                else{
+                    swal('Falta el 1234')
                 }
             }
             break;
         case 'F-6':
             if (barraLetras.innerText == 'ABCDEF') {
-                alert('Ya esta la F')
+                swal('Ya esta la F')
             } else if (barraLetras.innerText == 'ABCDE') {
                 barraLetras.innerText += 'F'
                 localStorage.setItem('letras', barraLetras.innerText)
             } else if (barraLetras.innerText == '' || barraLetras.innerText == 'A' || barraLetras.innerText == 'AB' || barraLetras.innerText == 'ABC' ||
                 barraLetras.innerText == 'ABCD') {
-                alert('Falta la letras ABCDE')
+                swal('Falta la letras ABCDE')
             }
             if (localStorage.getItem('letras') == 'ABCDEFGHIJ') {
                 if (barraNumeros.innerText == '123456') {
-                    alert('El 6 ya esta')
+                    swal('El 6 ya esta')
                 } else if (barraNumeros.innerText == '12345') {
                     barraNumeros.innerText += '6';
                     localStorage.setItem('numeros', barraNumeros.innerText)
+                }
+                else{
+                    swal('Falta el 12345')
                 }
             }
             break;
         case 'G-7':
             if (barraLetras.innerText == 'ABCDEFG') {
-                alert('Ya esta la G')
+                swal('Ya esta la G')
             } else if (barraLetras.innerText == 'ABCDEF') {
                 barraLetras.innerText += 'G'
                 localStorage.setItem('letras', barraLetras.innerText)
             } else if (barraLetras.innerText == '' || barraLetras.innerText == 'A' || barraLetras.innerText == 'AB' || barraLetras.innerText == 'ABC' ||
                 barraLetras.innerText == 'ABCD' || barraLetras.innerText == 'ABCDE') {
-                alert('Falta la letras ABCDEF')
+                swal('Falta la letras ABCDEF')
             }
             if (localStorage.getItem('letras') == 'ABCDEFGHIJ') {
                 if (barraNumeros.innerText == '1234567') {
-                    alert('El 7 ya esta')
+                    swal('El 7 ya esta')
                 } else if (barraNumeros.innerText == '123456') {
                     barraNumeros.innerText += '7';
                     localStorage.setItem('numeros', barraNumeros.innerText)
+                }
+                else{
+                    swal('Falta el 123456')
                 }
             }
             break;
         case 'H-8':
             if (barraLetras.innerText == 'ABCDEFGH') {
-                alert('Ya esta la H')
+                swal('Ya esta la H')
             } else if (barraLetras.innerText == 'ABCDEFG') {
                 barraLetras.innerText += 'H'
                 localStorage.setItem('letras', barraLetras.innerText)
             } else if (barraLetras.innerText == '' || barraLetras.innerText == 'A' || barraLetras.innerText == 'AB' || barraLetras.innerText == 'ABC' ||
                 barraLetras.innerText == 'ABCD' || barraLetras.innerText == 'ABCDE' || barraLetras.innerText == 'ABCDEF') {
-                alert('Falta la letras ABCDEF')
+                swal('Falta la letras ABCDEF')
             }
             if (localStorage.getItem('letras') == 'ABCDEFGHIJ') {
                 if (barraNumeros.innerText == '12345678') {
-                    alert('El 8 ya esta')
+                    swal('El 8 ya esta')
                 } else if (barraNumeros.innerText == '1234567') {
                     barraNumeros.innerText += '8';
                     localStorage.setItem('numeros', barraNumeros.innerText)
+                }
+                else{
+                    swal('Falta el 1234567')
                 }
             }
             break;
         case 'I-9':
             if (barraLetras.innerText == 'ABCDEFGHI') {
-                alert('Ya esta la I')
+                swal('Ya esta la I')
             } else if (barraLetras.innerText == 'ABCDEFGH') {
                 barraLetras.innerText += 'I'
                 localStorage.setItem('letras', barraLetras.innerText)
             } else if (barraLetras.innerText == '' || barraLetras.innerText == 'A' || barraLetras.innerText == 'AB' || barraLetras.innerText == 'ABC' ||
                 barraLetras.innerText == 'ABCD' || barraLetras.innerText == 'ABCDE' || barraLetras.innerText == 'ABCDEF' || barraLetras.innerText == 'ABCDEFH') {
-                alert('Falta la letras ABCDEFH')
+                swal('Falta la letras ABCDEFH')
             }
             if (localStorage.getItem('letras') == 'ABCDEFGHIJ') {
                 if (barraNumeros.innerText == '123456789') {
-                    alert('El 9 ya esta')
+                    swal('El 9 ya esta')
                 } else if (barraNumeros.innerText == '12345678') {
                     barraNumeros.innerText += '9';
                     localStorage.setItem('numeros', barraNumeros.innerText)
+                }else{
+                    swal('Falta el 12345678')
                 }
             }
             break;
@@ -177,14 +199,20 @@ const teclado = (event) => {
             if (barraLetras.innerText == 'ABCDEFGHI') {
                 barraLetras.innerText += 'J'
                 localStorage.setItem('letras', barraLetras.innerText)
-                alert('Letras Completadas')
+                swal("Buen Trabajo!", "Letras Completadas!", "success");
+            } else if (barraLetras.innerText == '' || barraLetras.innerText == 'A' || barraLetras.innerText == 'AB' || barraLetras.innerText == 'ABC' ||
+                barraLetras.innerText == 'ABCD' || barraLetras.innerText == 'ABCDE' || barraLetras.innerText == 'ABCDEF' || barraLetras.innerText == 'ABCDEFH' ||
+                barraLetras.innerText == 'ABCDEFGHI') {
+                swal('Falta la letras ABCDEFH')
             }
             if (localStorage.getItem('letras') == 'ABCDEFGHIJ') {
-                if (barraNumeros.innerText == '123456789') {
+                if(barraNumeros.innerText=='1234567890'){
+                    swal('Ya esta el 0')
+                }else if (barraNumeros.innerText == '123456789') {
                     barraNumeros.innerText += '0';
                     localStorage.setItem('numeros', barraNumeros.innerText)
-                    alert('Números completados')
-
+                    swal("Buen Trabajo!", "Números Completados!", "success");
+                    break;
                 }
             }
             break;
@@ -195,7 +223,7 @@ const teclado = (event) => {
             barraLetras.innerText = ''
             localStorage.setItem('letras', '')
             barraNumeros.innerText = ''
-            localStorage.setItem('numeros','')
+            localStorage.setItem('numeros', '')
         default:
     }
 }
